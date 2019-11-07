7e359e94e1f2fc5989683a8ee79dca684ce6a395
Kyojin <danielchoo55@gmail.com>
2019-11-07 12:39:20 -0500

    file checking

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@
 #
 # Author: Daniel Choo
 # Date:   10/28/19
+# URL:    https://www.github.com/kyoogoo/twump
 
 import os
 import sys
@@ -14,11 +15,44 @@ import itertools
 
 def authorize():
     """ authorize(): Reads in credentials JSON; verifies login.
-        Return:      N/A (throws error)
+                     If the credentials files does not exist, create a
+                     template for the user.
+        Return:      N/A (sys.exit if file is missing)
     """
-    with open("credentials.json") as cred:
-        data = json.load(cred)
-        
+    secret = {"ACCESS_KEY":      None,
+              "ACCESS_SECRET":   None,
+              "CONSUMER_KEY":    None,
+              "CONSUMER_SECRET": None}
+
+    values = ["ACCESS_KEY", "ACCESS_SECRET", "CONSUMER_KEY", "CONSUMER_SECRET"]
+
+    if os.path.exists("credentials.json"):
+        # Opening file and reading in the json information.
+        with open("credentials.json") as cred:
+            data = json.load(cred)
+            counter = 0
+            for key in secret:
+                secret[key] = data[values[counter]]
+                counter += 1
+            auth = tweepy.OAuthHandler(secret["CONSUMER_KEY"], secret["CONSUMER_SECRET"])
+            auth.set_access_token(secret["ACCESS_KEY"], secret["ACCESS_SECRET"])
+            pass
+
+    else:
+        # If the credentials.json is missing and the template exists... typo?
+        if os.path.exists("credentials_template.json"):
+            print("\nYou have either not properly named your file to 'credentials.json'")
+            print("Or there may be a typo in the file name. Else, this error should not be occurring.")
+            sys.exit(1)
+        else:
+            # If the credentials.json and template do not exist, create it!
+            with open("credentials_template.json", "w") as file:
+                file.write(json.dumps(secret, indent=4))
+                print("\nCreated new template.\nHowever, you are missing the necessary file to proceed")
+                print("Please fill in the blanks with the necessary information.")
+                print("Also, please rename the template file to 'credentials.json'")
+                sys.exit(1)
+
 
 def extract():
     """ extract(): Extracting data from Twitter
@@ -54,7 +88,7 @@ def main():
 
     # Lets go!
     elif usr_input == "y" or usr_input == "":
-        creds = authorize()
+        authorize()
         extract()
         print("Done.\n")
         return 0
