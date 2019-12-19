b1d38d16d0acb7ec7cdfa8051a3b19e77f5d8496
Kyojin <danielchoo55@gmail.com>
2019-12-18 21:35:28 -0500

    Hi Daniel

--- extract.py
+++ extract.py
@@ -9,9 +9,8 @@ import sys
 import time
 import json
 import tweepy
-import threading
 import itertools
-from src.twitter_json import Encode
+
 
 def authorize():
     """ authorize(): Reads in credentials JSON; verifies login.
@@ -54,12 +53,12 @@ def authorize():
         # Success! Ensure we are running a JSON parser.
         api = tweepy.API(auth)
         print("\n✧･ﾟ Authorized! ✧･ﾟ")
-        print("Logged in as: " + str(api.me().screen_name + "\n"))
+        print("Logged in as: " + str(api.me().screen_name))
         return api
 
     # If the file does not exist...
     else:
-        # If the credentials.json is missing and the template exists... typo?
+        # If the credentials.json is missing and the template exists, typo?
         if os.path.exists("credentials_template.json"):
             print("\nYou have either not properly named your file to 'credentials.json'")
             print("Or there may be a typo in the file name. Else, this error should not be occurring.")
@@ -73,64 +72,122 @@ def authorize():
                 print("Also, please rename the template file to 'credentials.json'")
                 sys.exit(-1)
 
+
+def print_tweet(status):
+    """ print_tweet(status): just prints the tweet text (void function)
+        Return:              N/A (void)
+    """
+    if 'retweeted_status' in status._json:
+        print("\n" + str(status.created_at) + ": " + status._json['retweeted_status']['full_text'])
+    else:
+        print("\n" + str(status.created_at) + ": " + status.full_text)
+
+
+def getUser(api):
+    """ getUser(username): retrieves user input from the user and 
+        Return:            user (API object)
+    """
+    # Attempt to see if the user exists on Twitter.
+    while True:
+        user_input = -1
+        username = input("\nEnter the Twitter Account to scrape: ")
+        try:
+            # Use streaming API on my own Twitter feed
+            if username == "self":
+                print("myself")
+                sys.exit(0)
+            # Use REST API on user's twitter timeline
+            else:
+                user = api.get_user(username)
+
+        # If username does not exist, retry or exit.
+        except tweepy.TweepError:
+            print("\nThat user does not exist.")
+            while user_input not in range(0, 2):
+                user_input = int(input("0 to exit, 1 to retry: "))
+
+            if user_input == 1:
+                continue
+            else:
+                print("\nGoodbye!")
+                sys.exit(-1)
+        # If we successfully retrieve a user, leave the loop.
+        else:
+            break
+
+    return user
+
+
 def extract(api, username):
     """ extract(): Extracting data from Twitter (REST API)
                    It will print out the messages it scrapes and append
                    them into a JSON file.
         Return:    N/A [None]
     """
-    # Attempt to see if the user exists on Twitter.
-    try:
-        # Use streaming API on my own Twitter feed
-        if username == "self":
-            print("myself")
-            sys.exit(0)
-        # Use REST API on user's twitter timeline
-        else:
-            api.get_user(username)
-    # If username does not exist...
-    except tweepy.TweepError:
-        print("\nThat user does not exist. Exiting program.\n")
-        sys.exit(-1)
-
     # Formatting file path; initializing variables.
-    counter = 0
     filename = username + ".json"
     path = "data/" + filename
-    print("This is the path: " + path)
-    print("\nRetreiving Tweets from " + str(username) + "...\n")
+    print("\nThis is the path: " + path)
+    print("Retreiving Tweets from " + str(username) + "...\n")
 
     """  parser=JSONParser()" currently still faces a bug. It will demonstrate this
          error code. 'JSONParser' object has no attribute 'model_factory.'
          Read more about it here: https://github.com/tweepy/tweepy/issues/538
          Thus, I've elected to design the JSON similarly to Twitter's format.
          However, I want to keep what I think is necessary. Trimming fat.
-    """
-    # If the path exists...
-    if os.path.exists(path):
-        pass
-    else:
-        with open(path, "w+") as file:
-            # Instantiating encode class.
-            encode = Encode()
 
-            # Iterating through user' tweets (REST)
-            for tweet in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended").items():
-                print(tweet)
-	            #            file.write(json.dumps(x, indent=4))
-                if counter >= 0:
+         Note: This has been resolved via a private variable: status._json.
+               I may come back and work on my own Twitter JSON parser, but that
+               is now indefinitly postponed.
+    """
+    check = False
+    counter = 0
+    tweet = ""
+    # Printing message, but also boolean value flips if the file exists. Required for loading JSON.
+    if os.path.exists(path):
+        print("Path already exists.")
+        with open(path, "r") as file:
+            for line in file:
+                if "full_text" in line:
+                    line = line.strip()                       # Strips whitespace
+                    line = line.replace('"full_text": ', "")  # Removes "full_text: "
+                    tweet = line[:-1]                         # Removes comma at the end
+                    tweet = tweet[1:-1]                       # Removes the quotes
                     break
                 else:
                     counter+=1
+        check = True
+        counter = 0
+    else:
+        print("Creating a new file.")
 
-            counter = 0
-            print("Extracting", end="")
-            for c in itertools.cycle(["."]):
-                print(c, flush=True, end="")
-                time.sleep(0.5)
-                if counter >= 3:
-                     break
+    # Automatically creates a new file!
+    with open(path, "a+") as file:
+        # Iterating through user' tweets (REST)
+        for status in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended").items():
+            # If the Tweet exists in our JSON file. No need to reappend it in. Break!
+            if check is True:
+                if status.full_text in tweet:
+                    print("Tweet already exists in JSON. Breaking.")
+                    break
+            print_tweet(status)
+
+            # Unfortunately, we have to set ensure_ascii to False because it does 
+            # not encode Emojis properly--which is a problem when comparing strings.
+            file.write(json.dumps(status._json, indent=4, ensure_ascii=False))
+            if counter >= 30:
+                break
+            else:
                 counter+=1
+
+#            counter = 0
+#            print("Extracting", end="")
+#            for c in itertools.cycle(["."]):
+#                print(c, flush=True, end="")
+#                time.sleep(0.5)
+#                if counter >= 3:
+#                     break
+#                counter+=1
 
 
 def main():
@@ -140,6 +197,34 @@ def main():
     # Initializing variables.
     valid = {"": "", "y": "y", "n": "n", "yes": "yes", "no": "no"}
     usr_input = "abc"
+    logo = """.   . .    .  .    .  .    .  .    .  .    .  .    .  .    .  .
+ .     .    .  .    .  .    .  .    ..S88888X88t. .    . . .      . .
+  .    .   .  ;;   .       .       . .XX88888X8@@88@t%..:%88%   . .
+     .   .   :@8t;   .  .    .  .   X888888X8X88S8X88888@88888:.      .  .
+  .    .    .X888:X   .   .   .   .%888888888888888888888 @@8@  .     .
+    .     .  88S888.X:  .   .   . :X88888%8 888888S@888%88888t    . .
+  .   .     .S@8 88888 8t.        X8888@8S8888S8888S88 8 88@t . .      .
+    .   . .  ;888 8888888@8@..  . 88888888S88 8 @888%X8888X.      .  .
+  .          :8 8888 888888@8 S88@8X8888888888888888888%88S .. .         .
+     . .  .  %X@888888S888 8S@888888888S8 888 888 @ @8XS8S: .   .  . .
+  .         .XX88888S8888X888 8S8 888888888888@888888S8888S    .   .    .
+    .  . .   ;8S8 8 @88 8 88X8%888%8S888888888 88888888888. . .  .    . 
+  .        . :S @88888888888 888888888XS888888 8888 888 @      .    .
+    . .  .   .:%8888888888S8888 888S88S8888 88888888888:    .  .     . .
+  .        .   ;%8888 @8888 888888S888 8888888888888 S     .   . .
+     . .        .888888S8888%8 888888S88888888S8 888:.  .    .      .
+  .      .  . . .888 888 @88888 8S8888888S888@888 8   .   .    .  .
+    .  .        ..@8888888888 8888S88S8 88888X8888      .   .        .
+  .      .  .  .  : X8%8S8 888888S8888888%8S8888:        .   . .  .
+    . .       .  .   . ;8@8 888 88888888 @88 8St     .     .
+  .     . .      .   ;888888S888S@88 8888 888t         .     .  .  .
+     .     .t%%SS@88 88888888 88888 @88X8888.  .  .  .   .    .     .
+  .    .    ;8X@888%88888 @88888 8S@8@ 888..           .   .     .
+    .    . . :S@888 888888 888S88888888 .    . .  . .    .   .  .   .
+  .   .      ...%t   .t88888888; S.%::  .  .     .    .    .      .
+    .   .  .  .        :t.:;;.. .  .         .      .   .    .  .
+  .      .   . .  . .  ...   ..      . . .     .  .   .   .   .    .  """
+    print(logo + "\n")
 
     # While the user would like to keep running the program...
     while usr_input not in valid:
@@ -156,9 +241,10 @@ def main():
         # Lets go!
         elif usr_input == "y" or usr_input == "":
             api = authorize()                        # Checking users' consumer/api key.
-            usr_input = input("Enter the Twitter Account to scrape: ")
-            extract(api, usr_input)                  # Extract information wanted information
-            print("\n\nDone.\n")
+#            print("Type 'self' to scrape your Twitter feed in live time.")
+            username = getUser(api).screen_name
+            extract(api, username)                  # Extract information wanted information
+            print("\nDone.\n")
             return 0                                 # Fini
         # This should not be possible. Literally. I think.
         else:
--- src/twitter_json.py
+++ src/twitter_json.py
@@ -2,10 +2,12 @@
 #
 # Author:  Daniel Choo
 # Date:    11/22/19
-# URL:     https://
+# URL:     https://www.github.com/kyoogoo/twump
 # License: MIT
 
+import os
 import tweepy
+import json
 
 
 class Encode:
@@ -23,71 +25,194 @@ class Encode:
 
         Thus, reiterating the previous paragraph, I have created this parser to
         truncate some variables Twitter provides and try to create a more
-        compressed, yet information-heavy JSON.
+        compressed, yet information-heavy JSON. Fundamentally, this parser will
+        detect the type of Tweet (tweet, retweet, extended, or quote), parse
+        the Status object and covert it into a dictionary accordingly, and
+        return the dictionary back to the program/user.
+
+        According to Twitter's documentation, a tweet's JSON is configured
+        in this format (https://developer.twitter.com/en/docs/tweets/data-
+        dictionary/overview/intro-to-tweet-json#retweet):
+
+        - Tweet (a Status object): the root of the JSON
+            + User: Account data.
+            + Entities: Includes URLS (https://twitter.com), Hashtags
+                        (#trashtag), Mentions (@adriyens), and Symbols ($)
+            x Extended Entities (Omitted from this parser. Too much data.)
+            x Places (Omitted from this parser. Although would be interesting.)
     """
     def __init__(self):
-        """ __init__(tweet):  Constructor; nothing needs to be initialized though.
+        """ __init__(self):  Constructor; nothing needs to be initialized though.
             Returns:          N/A [Nothing]
         """
         pass
 
-    def tweet(self, tweet):
-        """ tweet():          Encodes tweets (object: Status) into truncated
-                              key-pair values.
-            Returns:          tweet [Dict (multiple data types)]
+    def encode(self, status, t_type):
+        """ encode(status, t_type): Encodes the object into an JSON-appropriate
+                                    dictionary.
+            Returns:                N/A [Nothing]
+        """
+        try:
+            if t_type == "tweet":
+                tweet = tweet()
+            elif t_type == "retweet":
+                tweet = retweet()
+            elif t_type == "quote":
+                tweet = quote()
+            elif t_type == "extended":
+                tweet = extended()
+        except :
+	        
+
+
+
+        for key in status.items():
+            if key == 'user':
+                temp = users(key)
+                tweet[key] = temp
+            elif key == 'entities':
+                temp = entities(key)
+                tweet[key] = temp
+                
+    def tweet(self):
+        """ tweet(status): Encodes tweets (Status) into a dictionary.
+            Returns:       tweet (Dict)
         """
         tweet = {
-               'created_at': None,
-               'id_str': None,
-               'text': None,
-               'user': {
-                   'id': None,
-                   'name': None,
-                   'screen_name': None,
-                   'url': None,
-                   'description': None,
-               },
-               'place': {},
-               'entities': {
-                    'hashtags': [],
-                    'urls': [],
-                    'user_mentions': [],
-               },
-        }
+            'created_at': None,
+            'id_str':     None,
+            'text':       None,
+            'user': {
+                'id':          None,
+                'name':        None,
+                'screen_name': None,
+                'description': None,
+                'url':         None,
+            },
+            'place': {},
+            'coordinates': None,
+            'place': None,
+            'retweet_count': None,
+            'favorite_count': None,
+            'lang': None
+        },
         return tweet
 
-    def retweet(self, tweet):
-        """ retweet(tweet):   Encodes retweets into dictionaries.
-            Returns:          retweet [Dict (multiple data types)]
+    def user(self, username):
+        """ user(self, entity): Read in all the entities.
+            Return:                 entities (dict)
+        """
+        pass
+
+    def entities(self, entity):
+        """ entities(self, entity): Read in all the entities.
+            Return:                 entities (dict)
+        """
+
+        entities = {},
+
+        for key, val in entity.items():
+            entities[key] = val
+
+        return entities
+
+    def retweet(self):
+        """ retweet(tweet):   Returns retweets (Status) in dict format.
+            Returns:          retweet (Dict)
         """
         retweet = {
             'created_at': None,
-            'id': None,
-            'id_str': None,
-            'text': None,
-            'truncated': None,
+            'id_str':     None,
+            'text':       None,
+            'truncated':  None,
             'text_range': None,
             'entities': {
-                'hashtags': [],
-                'urls': [],
+                'hashtags':      [],
+                'urls':          [],
                 'user_mentions': [],
-            }
+            },
+            'user': {
+                'id':              None,
+                'id_str':          None,
+                'name':            None,
+                'screen_name':     None,
+                'location':        None,
+                'followers_count': None,
+                'friends_count':   None,
+                'verified':        None,
+            },
+            'retweeted_status': {
+                'created_at':         None,
+                'id':                 None,
+                'id_str':             None,
+                'full_text':          None,
+                'truncated':          None,
+                'display_text_range': [],
+                'entities': {
+                    'hashtags':      [],
+                    'symbols':       [],
+                    'user_mentions': [],
+                },
+                'user': {
+                    'id':             None,
+                    'id_str':         None,
+                    'name':           None,
+                    'screen_name':    None,
+                    'location':       None,
+                    'follower_count': None,
+                    'friends_count':  None,
+                    'verified':       None,
+                },
+                'is_quote':      None,
+                'retweet_count': None,
+            },
+        },
 
-        }
+        # Where the fun begins.
+
         return retweet
 
-    def extended(self, tweet):
-        """ extended(tweet):  Encodes extended tweets into dictionaries.
-            Returns:          extended [Dict (multiple data types)]
+    def extended(self):
+        """ extended(tweet):  Returns extended (Status) in dict format.
+            Returns:          extended (Dict)
         """
         extended = {
-        }
-        return extended
+            'created_at': None,
+            'id_str':     None,
+            'text':       None,
+            'truncated':  None,
+            'text_range': None,
+        },
 
-    def quote(self, tweet):
-        """ quote(tweet):    Encodes extended tweets into dictionaries.
-            Returns:          quotes [Dict (multiple data types)]
+       return extended
+
+    def quote(self):
+        """ quote(tweet):    Returns extended (Status) in dict format.
+            Returns:         quotes (Dict)
         """
         quote = {
-        }
+            "text": None,
+            "user": {
+                "screen_name": None,
+            },
+            "quoted_status": {
+                "text": None,
+                "user": {
+                    "screen_name": None,
+                },
+	            "place": {},
+	            "entities": {},
+	            "extended_entities": {}
+            },  
+	        "quoted_status_permalink": {
+		        "url": None,
+		        "expanded": None,
+		        "display": None,
+	        },    
+	        "place": {},
+	        "entities": {
+		        "urls": []
+	        }
+        },
+
         return quote
